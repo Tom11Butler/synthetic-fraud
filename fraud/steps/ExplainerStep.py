5ab257c0fcefef5bc39750c616728c8d459a0069
Tom Butler <tombutler21@hotmail.co.uk>
2021-08-28 14:23:44 +0100

    Displays force plots for example instances of a TP, FP, TN and FN. Used to interpret the most important features for that particular prediction.

--- fraud/steps/ExplainerStep.py
+++ fraud/steps/ExplainerStep.py
@@ -4,28 +4,66 @@ Defines the explainer class
 TODO: fill in class method definitions
 """
 
+import shap
+
 class ExplainerStep():
     """
     Object to explain model predictions
     """
     
-    def __init__(self, explainer, transformers):
+    def __init__(self, explainer, transformers, feature_columns):
         self._explainer = explainer
         self._transformers = transformers
+        self._feature_columns = feature_columns
+        
+        self._predictions = None
         
         
     def _inverse_transform(self, col, val):
-        pass
+        return self._transformers[col]\
+                .inverse_transform(val.reshape(-1,1))[0][0]
     
     def _report_most_important_features(self, instance):
-        pass
+        """
+        Reports the top five most important features for a given local
+        prediction.
+        """
+        
+        shap_values = self._explainer.shap_values(instance)
+        
+        df_shap = pd.DataFrame(
+            shap_values[1].reshape(-1,1)
+            , index=cols
+            , columns=['shap_value']
+        )
+        df_shap['abs_shap_value'] = abs(df_shap['shap_value'])
+        df_shap.sort_values('abs_shap_value', ascending=False, inplace=True)
+        
+        
     
-    def _shape_force_plot_instance(self, instance):
-        pass
+    def _shap_force_plot_instance(self, instance):
+        display(shap.force_plot(
+            self._explainer.expected_value[1]
+            , self._explainer.shap_values(instance)[1]
+            , instance
+            , self._feature_columns))
     
     def report_tp_fp_tn_fs_explanations(self, predictions, X):
-        pass
-    
+        self._predictions = predictions
+        
+        tp = self._predictions.query("true==pred").query("true==1").index[0]
+        fp = self._predictions.query("true!=pred").query("true==1").index[0]
+        tn = self._predictions.query("true==pred").query("true==0").index[0]
+        fn = self._predictions.query("true!=pred").query("true==0").index[0]
+        
+        for pred_type, index in zip(['tp','fp','tn','fn'], [tp,fp,tn,fn]):
+            print(f"Example explanation for a {pred_type} prediction ...")
+            print("----------------")
+            
+            instance = X[index]
+            
+            # self._report_most_important_features(instance)
+            self._shap_force_plot_instance(instance)
     
     def all_shap_values(self, X):
         pass
