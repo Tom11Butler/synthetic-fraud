59d38df462843072ecb615f49434c5d1d622e41a
Tom Butler <tombutler21@hotmail.co.uk>
2021-08-28 13:40:43 +0100

    Updating the configuration file of parameters

--- fraud/config/config.py
+++ fraud/config/config.py
@@ -4,6 +4,6 @@ Master file for the parameters dictionary
 
 PARAMS = dict()
 
-PARAMS['input_dir'] = 'data/'
-PARAMS['input_data'] = f"{PARAMS['input_dir']}synthetic_fraud.csv"
-PARAMS['output_dir'] = 'output/'
+PARAMS['input_path'] = 'data/'
+PARAMS['input_data'] = f"{PARAMS['input_path']}synthetic_fraud.csv"
+PARAMS['output_path'] = 'output/'
